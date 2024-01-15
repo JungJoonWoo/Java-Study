913c6c8bcdcddc45f10be27e2cc9ef3521d6e366
JungJoonWoo <jjw2030@ajou.ac.kr>
2024-01-15 16:59:44 +0900

    Jdbc

--- spring-start/hello-spring/src/main/java/hello/hellospring/Controller/MemberController.java
+++ spring-start/hello-spring/src/main/java/hello/hellospring/Controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.Controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -11,5 +17,26 @@ public class MemberController {
     @Autowired//컨트롤러와 서비스를 연결
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new") // 데이터를 조회할 떄
+    public String creatForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new") //데이터를 전달할 때
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";//home화면으로 감
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
