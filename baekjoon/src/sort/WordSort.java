8a5e49f124865feeffdc87c3f5ec1766b6a3b2ba
JungJoonWoo <jjw2030@ajou.ac.kr>
2024-01-25 05:25:34 +0900

    sort

--- baekjoon/src/sort/WordSort.java
+++ baekjoon/src/sort/WordSort.java
@@ -1,11 +1,11 @@
 package sort;
 /*
-
+ Arrays.sort()에서 정렬 알고리즘에 의해 위치를 바꾸고, 0 이나 음의 정수인 경우는 두 객체의 위치는 바뀌지 않는다.
  */
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class WordSort {
     public static void main(String[] args) throws Exception {
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
