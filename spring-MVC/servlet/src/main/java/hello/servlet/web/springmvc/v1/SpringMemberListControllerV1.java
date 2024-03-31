f64e790f6eaa1835d7086d45aacefc6f8e778ae5
JungJoonWoo <jjw2030@ajou.ac.kr>
2024-04-01 00:47:27 +0900

    mvc

--- spring-MVC/servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
+++ spring-MVC/servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -2,8 +2,6 @@ package hello.servlet.web.springmvc.v1;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,12 +9,18 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 @Controller
-public class SpringMemberFormControllerV1 {
+public class SpringMemberListControllerV1 {
 
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
 
-
-    @RequestMapping("/springmvc/v1/members/new-form")
+    @RequestMapping("/springmvc/v1/members")
     public ModelAndView process() {
-        return new ModelAndView("new-form");
+
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
     }
+
 }
