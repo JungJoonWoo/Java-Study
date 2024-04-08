53705e7c50d56e0a155bdc46853f31bf697fbd59
JungJoonWoo <jjw2030@ajou.ac.kr>
2024-04-08 22:46:02 +0900

    mvc

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -23,27 +23,20 @@ public class RequestBodyJsonController {
 
   @PostMapping("/request-body-json-v1")
   public void requestBodyJson1V1(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
     ServletInputStream inputStream = request.getInputStream();
     String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
-
     log.info("messageBody={}", messageBody);
     HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
     log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
-
     response.getWriter().write("ok");
-
   }
 
   @ResponseBody
   @PostMapping("/request-body-json-v2")
   public String requestBodyJsonV2(@RequestBody String messageBody) throws IOException {
-
-
     log.info("messageBody={}", messageBody);
     HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
     log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
-
     return "ok";
   }
 
--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -24,40 +24,27 @@ public class RequestBodyStringController {
   public void requestBodyString(HttpServletRequest request, HttpServletResponse response) throws IOException {
     ServletInputStream inputStream = request.getInputStream();
     String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
-
     log.info("message={}", messageBody);
-
     response.getWriter().write("ok");
-
   }
   @PostMapping("/request-body-string-v2")
   public void requestBodyString(InputStream inputStream, Writer responseWriter) throws IOException {
     String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
-
     log.info("message={}", messageBody);
-
     responseWriter.write("ok");
-
   }
 
   @PostMapping("/request-body-string-v3")
   public HttpEntity<String> requestBodyString(HttpEntity<String> httpEntity) throws IOException {
-
     String body = httpEntity.getBody();
-
     log.info("message={}", body);
-
     return new HttpEntity<>("ok");
-
   }
 
   @ResponseBody
   @PostMapping("/request-body-string-v4")
   public String requestBodyString(@RequestBody String messageBody) throws IOException {
-
     log.info("message={}", messageBody);
-
     return "ok";
-
   }
 }
